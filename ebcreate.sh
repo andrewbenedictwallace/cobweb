a134acebb8f7aba73f7631487c326a80b3298885
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-09-14 11:32:38 -0700

    Deleted everything except README.md, which directs users to the new home. Previous state should be preserved in git history, "deprecated" branch.

--- ebcreate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-eb create cobweb-dev \
-    --database \
-    --database.engine postgres \
-    --database.instance db.t2.micro \
-    --database.size 5 \
-    --database.username cobweb \
-    
--- start-eb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-python3 manage.py migrate --noinput
-# python3 manage.py collectstatic --noinput
-python3 manage.py archive-it
-python3 manage.py ait-resources.py &
-
-exec uwsgi uwsgi.ini
